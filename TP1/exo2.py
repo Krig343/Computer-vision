6cb218df1c0c5e1b776b7cd03fbf7c12601ae883
HORNY GREGORY <gregory.horny@etu.unistra.fr>
2021-09-28 12:21:11 +0200

    Ajout de l'exercice 2

--- TP1/exo2.py
+++ TP1/exo2.py
@@ -1,7 +1,22 @@
 import cv2 as cv
 import sys
+import matplotlib.pyplot as plt
 
 img_bgr = cv.imread(sys.argv[1])
 
 if img_bgr is None:
 	sys.exit("Could not read the image.")
+
+b_histo = cv.calcHist([img_bgr], [0], None, [256], [0,255])
+g_histo = cv.calcHist([img_bgr], [1], None, [256], [0,255])
+r_histo = cv.calcHist([img_bgr], [2], None, [256], [0,255])
+
+cv.imshow("Image",img_bgr)
+cv.waitKey(0)
+
+plt.plot(b_histo)
+plt.plot(g_histo)
+plt.plot(r_histo)
+plt.show()
+
+cv.destroyAllWindows()
