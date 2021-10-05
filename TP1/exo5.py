537c4dfeed591b0f1c13fad210bf5d421fab9a6d
HORNY GREGORY <gregory.horny@etu.unistra.fr>
2021-10-05 08:54:09 +0200

    Ajout de la première partie de l'exercice 5

--- TP1/exo5.py
+++ TP1/exo5.py
@@ -3,7 +3,7 @@ import sys
 import matplotlib.pyplot as plt
 
 vid = cv.VideoCapture(0)
-
+plt.ion()
 while(vid.isOpened()):
       
     ret, frame = vid.read()
@@ -12,9 +12,26 @@ while(vid.isOpened()):
     cv.imshow('frame', gray)
       
     histo = cv.calcHist([gray], [0], None, [256], [0,256])
+    plt.plot(histo)
+    plt.show()
+    plt.pause(0.01)
+    plt.clf()
+
+    b_histo = cv.calcHist([vid], [0], None, [256], [0,256])
+    g_histo = cv.calcHist([vid], [1], None, [256], [0,256])
+    r_histo = cv.calcHist([vid], [2], None, [256], [0,256])
+
+    cv.imshow("RGB", vid)
+
+    plt.plot(b_histo,'b')
+    plt.plot(g_histo,'g')
+    plt.plot(r_histo,'r')
+    plt.show()
+    plt.pause(0.01)
+    plt.clf()
 
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
-  
+
 vid.release()
 cv.destroyAllWindows()
