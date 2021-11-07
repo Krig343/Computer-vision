44bd4dbc7319db0458f442f9603bf09913680e1b
HORNY GREGORY <gregory.horny@etu.unistra.fr>
2021-11-07 13:15:01 +0100

    Respect de la mise en page et des affichage vis à vis du sujet

--- TP3/canny.py
+++ TP3/canny.py
@@ -3,17 +3,31 @@ import sys
 import numpy as np
 import math
 import queue
+np.set_printoptions(threshold=sys.maxsize)
 
 if len(sys.argv) < 5:
-    sys.exit("Format attendu : " + sys.argv[0] + " <fichier image>" + " valeur de sigma" + " alpha" + " beta")
+    print("Nombre d'argument invalide")
+    sys.exit("Format attendu : " + sys.argv[0] + " <fichier image>" + " valeur de sigma" + " seuil haut" + " seuil bas")
+
+if int(sys.argv[2]) <= 0:
+    sys.exit("La valeur de sigma doit être posiitve")
+
+if float(sys.argv[3]) < 0 or float(sys.argv[3]) > 1:
+    sys.exit("alpha doit avoir une valeur comprise entre 0 et 1")
+
+if float(sys.argv[4]) < 0 or float(sys.argv[4]) > 1:
+    sys.exit("beta doit avoir une valeur comprise entre 0 et 1")
+
+if float(sys.argv[3]) < float(sys.argv[4]):
+    sys.exit("alpha doit être supérieur à beta")
 
 img = cv.imread(sys.argv[1])
 img_origin = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 
-cv.imshow("Image originele",img_origin)
-
-def gaussianFiltering (img, sigma): #A changer
-    n = math.floor(3/sigma)
+def gaussianFiltering (img, sigma):
+    n = math.floor(3*sigma)
+    if n%2 == 0:
+        n+=1
     return cv.GaussianBlur(img,(n,n),sigma)
 
 def computeGx(img):
@@ -54,36 +68,32 @@ def removeNonMaxima(grad_m, grad_d):
                 grad_d[x][y] -= Pi
             if grad_d[x][y] >= -Pi/8 and grad_d[x][y] < Pi/8:
                 if x-1 > 0 and grad_m[x-1][y] > grad_m[x][y]:
-                        img[x][y] = 0
+                    img[x][y] = 0
+                elif x+1 < xmax and grad_m[x+1][y] > grad_m[x][y]:
+                    img[x][y] = 0
                 else:
-                    if x+1 < xmax and grad_m[x+1][y] > grad_m[x][y]:
-                        img[x][y] = 0
-                    else:
-                        img[x][y] = grad_m[x][y]
+                    img[x][y] = grad_m[x][y]
             elif grad_d[x][y] >= Pi/8 and grad_d[x][y] < 3*Pi/8:
                 if (x-1 > 0 and y+1 < ymax) and grad_m[x-1][y+1] > grad_m[x][y]:
-                        img[x][y] = 0
+                    img[x][y] = 0
+                elif (y-1 > 0 and x+1 < xmax) and grad_m[x+1][y-1] > grad_m[x][y]:
+                    img[x][y] = 0
                 else:
-                    if (y-1 > 0 and x+1 < xmax) and grad_m[x+1][y-1] > grad_m[x][y]:
-                        img[x][y] = 0
-                    else:
-                        img[x][y] = grad_m[x][y]
+                    img[x][y] = grad_m[x][y]
             elif grad_d[x][y] >= 3*Pi/8 and grad_d[x][y] < 5*Pi/8:
                 if y+1 < ymax and grad_m[x][y+1] > grad_m[x][y]:
-                        img[x][y] = 0
+                    img[x][y] = 0
+                elif y-1 > 0 and grad_m[x][y-1] > grad_m[x][y]:
+                    img[x][y] = 0
                 else:
-                    if y-1 > 0 and grad_m[x][y-1] > grad_m[x][y]:
-                        img[x][y] = 0
-                    else:
-                        img[x][y] = grad_m[x][y]
+                    img[x][y] = grad_m[x][y]
             elif grad_d[x][y] >= 5*Pi/8 and grad_d[x][y] < 7*Pi/8:
                 if (y-1 > 0 and x-1 > 0) and grad_m[x-1][y-1] > grad_m[x][y]:
-                        img[x][y] = 0
+                    img[x][y] = 0
+                elif (y+1 < ymax and x+1 < xmax) and grad_m[x+1][y+1] > grad_m[x][y]:
+                    img[x][y] = 0
                 else:
-                    if (y+1 < ymax and x+1 < xmax) and grad_m[x+1][y+1] > grad_m[x][y]:
-                        img[x][y] = 0
-                    else:
-                        img[x][y] = grad_m[x][y]
+                    img[x][y] = grad_m[x][y]
     return img
 
 def computeThresholds(grad_maxima,  alpha,  beta):
@@ -145,20 +155,31 @@ def hysteresisThresholding(grad_maxima, tLow, tHigh):
     return canny
 
 blur = gaussianFiltering(img_origin, int(sys.argv[2]))
-cv.imshow("Image blur",img_origin)
+blurFile = open("Filtre gaussien.txt","w")
+blurFile.write(str(blur))
+blurFile.close()
 
-mag = computeMagnitude(computeGx(blur),computeGy(blur))
-cv.imshow("Image gradient magnitude",mag)
+Gx = computeGx(blur)
+Gy = computeGy(blur)
 
-dir = computeDirection(computeGx(blur),computeGy(blur))
-cv.imshow("Image gradient direction",dir)
+mag = computeMagnitude(Gx,Gy)
+magFile = open("Magnitude du gradient.txt","w")
+magFile.write(str(mag))
+magFile.close()
+
+dir = computeDirection(Gx,Gy)
 
 gmax = removeNonMaxima(mag,dir)
-cv.imshow("Image sans les maximas",gmax)
 
 high, low = computeThresholds(gmax, float(sys.argv[3]), float(sys.argv[4]))
 
+print("Seuil haut : " + str(high))
+print("Seuil bas : " + str(low))
+
 canny = hysteresisThresholding(gmax, low, high)
+cannyFile = open("Filtre de conny.txt","w")
+cannyFile.write(str(canny))
+cannyFile.close()
 cv.imshow("Filtre de canny",canny)
 
 cv.waitKey(0)
