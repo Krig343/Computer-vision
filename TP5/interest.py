d9085dc0697b0c630483c83777ed9cdd8e504ccf
Grégory Horny <gregory.horny@etu.unistra.fr>
2021-12-02 17:37:45 +0100

    Ajout de la fonction noise

--- TP5/interest.py
+++ TP5/interest.py
@@ -1,45 +1,61 @@
 import cv2 as cv
 import numpy as np
 import sys
+np.set_printoptions(threshold=sys.maxsize)
 
 def scale (img, t):
     shape = np.shape(img)
-    size = (shape[1],shape[0])
+    size = (shape[0],shape[1]) #peut-être inverser
     V = np.float32([[t, 0, 1], [0, t, 1]])
     return cv.warpAffine(img, V, size)
 
 def rotation (img, a):
     height = img.shape[0]
     width = img.shape[1]
-    center = (height/2, width/2) #centre pas correct
+    center = (height/2, width/2)
     M = cv.getRotationMatrix2D(center, a, 1)
     shape = np.shape(img)
     size = (shape[1],shape[0])
     return cv.warpAffine(img, M, size)
 
-def computeHarris (img, angle, taille):
+def computeHarris (img, angle, taille, noise):
     img = scale (img, taille)
     img = rotation (img, angle)
+    img = addNoise (img, noise)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     gray = np.float32(gray)
     harrised = cv.cornerHarris(gray,3,3,0.04)
     harrised = cv.dilate(harrised,None)
-    img[harrised>0.01*harrised.max()]=[0,0,255]        
+    img[harrised>0.01*harrised.max()]=[0,0,255]
     cv.imshow('Harris cv',img)
 
-def computeIx(img):
-    return cv.Sobel(img, -1, 1, 0, 3)
+def computeIx(image):
+    return cv.Sobel(image, -1, 1, 0, 3)
 
 def computeIy(img):
     return cv.Sobel(img, -1, 0, 1, 3)
 
+def computeSommeI (image):
+    Ix = computeIx(image)
+    Iy = computeIy(image)
+    #A finir
+
+def computeR (mat):
+    pass#A finir
+
+def addNoise (image, n):
+    size = np.shape(image)
+    return np.random.normal(0.0, n, size)
+
 def refresh (img, angle, taille, version, noise):
     if version == 0:
-        computeHarris (img, angle, taille)
+        computeHarris (img, angle, taille, noise)
     elif version == 1:
         pass #mon harris
     elif version == 2:
         pass #computeShiTomasi()
+    elif version == 3:
+        pass #SIRF
 
 def update_angle (a):
     global angle
@@ -59,18 +75,19 @@ def update_version (v):
 def update_noise (n):
     global noise
     noise = n
-    refresh (img, angle, taille, noise)
+    refresh (img, angle, taille, version, noise)
 
 taille = 1
 angle = 0
 version = 0
 noise = 0
-img = cv.imread(sys.argv[1])
+img_source = cv.imread(sys.argv[1])
+img = cv.resize(img_source, (500,500))
 
 cv.namedWindow('Harris cv')
 cv.createTrackbar('angle', 'Harris cv',0,360, update_angle)
 cv.createTrackbar('scale', 'Harris cv',50,100, update_scale)
-cv.createTrackbar('version', 'Harris cv',0,2, update_version)
+cv.createTrackbar('version', 'Harris cv',0,3, update_version)
 cv.createTrackbar('bruit', 'Harris cv',0,100, update_noise)
 
 refresh(img, angle, taille, version, noise)
