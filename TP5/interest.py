b71c5a9687a8c700c34fbb127f7eb36bb87389b6
Grégory Horny <gregory.horny@etu.unistra.fr>
2021-12-03 17:08:45 +0100

    Ajout de la tranformation perspective

--- TP5/interest.py
+++ TP5/interest.py
@@ -1,29 +1,33 @@
 import cv2 as cv
 import numpy as np
 import sys
-from matplotlib import pyplot as plt
-
-from numpy.core.defchararray import add
-np.set_printoptions(threshold=sys.maxsize)
 
 def scale (img, t):
     shape = np.shape(img)
     size = (shape[0],shape[1]) #peut-être inverser
-    V = np.float32([[t, 0, 1], [0, t, 1]])
-    return cv.warpAffine(img, V, size)
+    M = np.float32([[t, 0, 1], [0, t, 1]])
+    return cv.warpAffine(img, M, size)
 
 def rotation (img, a):
     height = img.shape[0]
     width = img.shape[1]
     center = (height/2, width/2)
     M = cv.getRotationMatrix2D(center, a, 1)
-    shape = np.shape(img)
-    size = (shape[1],shape[0])
+    size = (height,width) #peut-être inverser
     return cv.warpAffine(img, M, size)
 
-def computeHarris (img, angle, taille, noise):
-    img = scale(img, taille)
+def perspective (img, p, q, k, l):
+    size = np.shape(img)
+    pts1 = np.float32([p,q,k,l])
+    pts2 = np.float32([[0,0],[size[0],0],[0,size[1]],[size[0],size[1]]])
+    M = cv.getPerspectiveTransform(pts1,pts2)
+    return cv.warpPerspective(img, M, (size[0],size[1]))
+
+def computeHarris (img):
+    img = scale (img, taille)
     img = rotation (img, angle)
+    if persp:
+        img = perspective (img, p1, p2, p3, p4)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     gray = np.float32(gray)
     gray = addNoise(gray, noise)
@@ -33,9 +37,11 @@ def computeHarris (img, angle, taille, noise):
     img[harrised>0.01*harrised.max()]=[0,0,255]
     cv.imshow('Harris cv',img)
 
-def computeShiTomasi (img, angle, taille, noise):
+def computeShiTomasi (img):
     img = scale(img, taille)
     img = rotation (img, angle)
+    if persp:
+        img = perspective (img, p1, p2, p3, p4)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     gray = np.float32(gray)
     gray = addNoise(gray, noise)
@@ -47,9 +53,11 @@ def computeShiTomasi (img, angle, taille, noise):
         cv.circle(img,(x,y),3,255,-1)
     cv.imshow('Harris cv', img)
 
-def computeSIFT (img, angle, taille, noise):
+def computeSIFT (img):
     img = scale(img, taille)
     img = rotation (img, angle)
+    if persp:
+        img = perspective (img, p1, p2, p3, p4)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     gray = np.float32(gray)
     gray = addNoise(gray, noise)
@@ -70,65 +78,93 @@ def computeSIFT (img, angle, taille, noise):
 #     Iy = computeIy(image)
 #     #A finir
 
-def computeR (mat):
-    pass#A finir
+# def computeR (mat):
+#     pass#A finir
 
-def addNoise (image, n):
-    size = np.shape(image)
+def addNoise (img, n):
+    size = np.shape(img)
     M = np.random.normal((255/2), n, size)
-    image = np.add(image, M)
+    img = np.add(img, M)
     for x in range(size[0]):
         for y in range(size[1]):
-            if image[x,y] < 0:
-                image[x,y] = 0
-            if image[x,y] > 255:
-                image[x,y] = 255
-    return image
+            if img[x,y] < 0:
+                img[x,y] = 0
+            if img[x,y] > 255:
+                img[x,y] = 255
+    return img
 
-def refresh (img, angle, taille, version, noise):
+def refresh (img):
     if version == 0:
-        computeHarris (img, angle, taille, noise)
+        computeHarris (img)
     elif version == 1:
         pass #mon harris
     elif version == 2:
-        computeShiTomasi (img, angle, taille, noise)
+        computeShiTomasi (img)
     elif version == 3:
-        computeSIFT (img, angle, taille, noise)
+        computeSIFT (img)
+    elif version == 4:
+        pass #ORB
 
 def update_angle (a):
     global angle
     angle = a
-    refresh (img, angle, taille, version, noise)
+    refresh (img)
 
 def update_scale (s):
     global taille
     taille = float(s+50)/100
-    refresh (img, angle, taille, version, noise)
+    refresh (img)
 
 def update_version (v):
     global version
     version = v
-    refresh (img, angle, taille, version, noise)
+    refresh (img)
 
 def update_noise (n):
     global noise
     noise = n
-    refresh (img, angle, taille, version, noise)
+    refresh (img)
+
+def update_perspective (p):
+    global persp, p1, p2, p3, p4
+    if persp == 0:
+        p1x = input("Entrez les coordonées x du premier point : ")
+        p1y = input("Entrez les coordonées y du premier point : ")
+        p1 = (int(p1x), int(p1y))
+        p2x = input("Entrez les coordonées x du deuxième point : ")
+        p2y = input("Entrez les coordonées y du deuxième point : ")
+        p2 = (int(p2x), int(p2y))
+        p3x = input("Entrez les coordonées x du troisième point : ")
+        p3y = input("Entrez les coordonées y du troisième point : ")
+        p3 = (int(p3x), int(p3y))
+        p4x = input("Entrez les coordonées x du quatrième point : ")
+        p4y = input("Entrez les coordonées y du quatrième point : ")
+        p4 = (int(p4x), int(p4y))
+    persp = p
+    refresh (img)
 
 taille = 1
 angle = 0
 version = 0
 noise = 0
+persp = 0
 img_source = cv.imread(sys.argv[1])
 img = cv.resize(img_source, (500,500))
+height = img.shape[0]
+width = img.shape[1]
+p1 = (0,0)
+p2 = (width,0)
+p3 = (0,height)
+p4 = (width,height)
 
 cv.namedWindow('Harris cv')
 cv.createTrackbar('angle', 'Harris cv',0,360, update_angle)
 cv.createTrackbar('scale', 'Harris cv',50,100, update_scale)
-cv.createTrackbar('version', 'Harris cv',0,3, update_version)
+cv.createTrackbar('version', 'Harris cv',0,4, update_version)
 cv.createTrackbar('bruit', 'Harris cv',0,100, update_noise)
+cv.createTrackbar('perspective', 'Harris cv',0,1, update_perspective)
 
-refresh(img, angle, taille, version, noise)
+refresh(img)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
